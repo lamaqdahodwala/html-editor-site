109b2f4d6d7917adf7cfe7a503ec8d6f5d7eba1f
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-07 18:56:13 -0500

    update gitignore

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,3 +1,42 @@
+import { PrismaClient } from '.prisma/client';
+import { Actions } from '@sveltejs/kit';
+import * as jose from 'jose'
 export const actions: Actions = {
-    
+	default: async (event) => {
+		let data = await event.request.formData();
+		let username = data.get('username');
+		let password = data.get('password');
+
+		let prisma = new PrismaClient();
+
+		try {
+			let user = await prisma.user.findUniqueOrThrow({
+				where: {
+					username: username
+				}
+			});
+
+            if (user.password === password){
+                let jwt = await jose.SignJWT({
+                    
+                })
+            } else {
+                return {
+                    success: false, 
+                    error: "The password is incorrect"
+                }
+            }
+		} catch (error) {
+			return {
+				success: false,
+				error: 'An account with this username was not found'
+			};
+		}
+
+        prisma.$disconnect()
+
+        
+
+
+	}
 };
