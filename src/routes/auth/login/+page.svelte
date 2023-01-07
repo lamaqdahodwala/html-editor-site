65be6597b3d0134d338316884a870a8fefffbc57
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-06 20:42:01 -0500

    added forms for authentication

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,13 @@
+<form method="post">
+    <label for="username">
+        <p>Username</p>
+        <input type="text"  id="" name="username">
+    </label>
+
+    <label for="password">
+        <p>Password</p>
+        <input type="password" name="password" id="">
+    </label>
+
+    <button type="submit">Log in</button>
+</form>
