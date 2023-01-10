f7499ea964232a5c8c8f47df903274143b3b28f0
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-09 20:07:00 -0500

    creating the editor guys

--- src/routes/editor/[username]/[title]/+page.server.ts
+++ src/routes/editor/[username]/[title]/+page.server.ts
@@ -1,18 +1,40 @@
-import { PrismaClient } from "@prisma/client";
-import type { PageServerLoad } from "./$types";
+import { PrismaClient } from '@prisma/client';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-    const prisma = new PrismaClient()
-    const pen_data = await prisma.user.findUnique({
-        where: {
-            username: event.params.username
-        },
-        include: {
-            pens: {
-                where: {
-                    title: event.params.title
-                }
-            }
+	const prisma = new PrismaClient();
+	try {
+		const pen_data = await prisma.user.findUnique({
+			where: {
+				username: event.params.username
+			},
+			include: {
+				pens: {
+					where: {
+						title: event.params.title
+					}
+				}
+			}
+		});
+
+        console.log(pen_data)
+        if (pen_data?.pens.length !== 1){
+            throw error(404, "Not found")
         }
-    })
+
+        let pen = pen_data.pens[0]
+
+        return {
+            username: event.params.username,
+            title: event.params.title,
+            html: pen.html,
+            css: pen.css, 
+            js: pen.js
+        }
+
+        
+	} catch (e) {
+		throw error(404, 'Not found');
+	}
 };
--- src/routes/editor/[username]/[title]/+page.svelte
+++ src/routes/editor/[username]/[title]/+page.svelte
@@ -1,7 +1,22 @@
-<script>
-	let html = '';
-	let css = '';
-	let js = '';
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	
+	import type { PageData } from "./$types";
+
+
+	export let data: PageData
+
+    let html : String 
+    let css : String
+    let js : String
+    onMount(() => {
+        html = data.html
+        css = data.css
+        js = data.js
+    })
+
+    
 </script>
 
 <div class="grid grid-cols-3 gap-3">
@@ -9,6 +24,10 @@
 	<textarea bind:value={css} name="" id="" cols="30" rows="10" class="border-2 border-black" />
 	<textarea bind:value={js} name="" id="" cols="30" rows="10" class="border-2 border-black" />
 	<div class="col-span-3">
-        <iframe src="/" frameborder="0" class="border-2 w-full h-full" />
+        <iframe src="/editor/{data.username}/{data.title}/view" title="Live View" frameborder="0" class="border-2 w-full h-full" />
     </div>
 </div>
+
+<p>{html}</p>
+<p>{css}</p>
+<p>{js}</p>
