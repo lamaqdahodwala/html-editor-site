39644fbcfa884baaa27e7aa1740a4ded6712a33c
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-11 19:11:47 -0500

    added post creation routes

--- src/routes/(app)/posts/create/+page.server.ts
+++ src/routes/(app)/posts/create/+page.server.ts
@@ -1,7 +1,98 @@
-import type { Actions } from "@sveltejs/kit";
-
+import { Prisma, PrismaClient } from '@prisma/client';
+import { error, redirect, type Actions } from '@sveltejs/kit';
+import * as jose from 'jose';
+import type { PageServerLoad } from './$types';
 export const actions: Actions = {
-    default: async(event) => {
+	default: async (event) => {
+		let jwt = event.cookies.get('jwt');
+		try {
+			let decoded = await jose.jwtVerify(jwt, new TextEncoder().encode(process.env['KEY']));
+
+			let prisma = new PrismaClient();
+
+			let data = await event.request.formData();
+
+			let title = data.get('title');
+			let content = data.get('content');
+			let pen_title = data.get('pen_title');
+
+			let user = await prisma.user.findUnique({
+				where: {
+					username: decoded.payload.aud
+				},
+				include: {
+					pens: {
+						where: {
+							title: pen_title
+						}
+					}
+				}
+			});
+
+			if (user.pens.length !== 1) {
+				throw error(500, 'Error');
+			}
+
+			let pen = user.pens[0];
+
+			if (decoded.payload.aud !== pen.owner.username) {
+				throw error(403, 'Not authorized');
+			}
+
+			let post = await prisma.post.create({
+				data: {
+					title: title,
+					content: content,
+					pen: {
+						connect: {
+							id: pen.id
+						}
+					},
+					owner: {
+						connect: {
+							username: decoded.payload.aud
+						}
+					}
+				}
+			});
+
+			return {
+				success: true,
+				error: null
+			};
+		} catch (error) {
+			throw redirect(302, '/auth/login');
+		}
+	}
+};
+export const load: PageServerLoad = async (event) => {
+	let jwt = event.cookies.get('jwt');
+	try {
+
+		if (jwt === undefined){
+			throw new Error()
+		}
+		let decoded = await jose.jwtVerify(jwt, new TextEncoder().encode(process.env['KEY']));
+
+        let username = decoded.payload.aud
+
+        let  prisma = new PrismaClient()
+
+        let pen_titles = await prisma.user.findUnique({
+            where: {
+                username: username
+            },
+            include: {
+                pens: {
+                    select: {
+                        title: true
+                    }
+                }
+            }
+        })
+
         
+	} catch (error) {
+        throw redirect(302, "/auth/login")
     }
 };
---
+++ src/routes/(app)/posts/create/+page.svelte
@@ -0,0 +1,17 @@
+<form method="post">
+    <label for="title">
+        <p>Title</p>
+        <input type="text" name="title" id="">
+    </label>
+
+    <label for="content">
+        <p>Body</p>
+        <textarea name="content" id="" cols="30" rows="10"></textarea>
+    </label>
+
+    <select name="post_title" id="">
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+    </select>
+</form>
