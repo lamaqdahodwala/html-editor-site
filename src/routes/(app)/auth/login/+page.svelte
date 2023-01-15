ec98d4d969e662fa307896abd04d413b6ee91300
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-14 19:54:57 -0500

    fancy fonts

--- src/routes/(app)/auth/login/+page.svelte
+++ src/routes/(app)/auth/login/+page.svelte
@@ -2,19 +2,23 @@
 	export let form: FormData;
 </script>
 
-<form method="post">
-	<label for="username">
-		<p>Username</p>
-		<input type="text" id="" name="username" />
-	</label>
-
-	<label for="password">
-		<p>Password</p>
-		<input type="password" name="password" id="" />
-	</label>
-
-	<button type="submit">Log in</button>
-</form>
+<div class="grid place-items-center">
+	<div class="shadow-lg rounded-xl p-4">
+		<form method="post">
+			<label for="username">
+				<p class="m-2 text-xl text-center">Username</p>
+				<input type="text" id="" name="username" class="h-10 text-lg border-2 border-gray-200 outline-none focus:outline-none focus:border-gray-400 rounded-lg" />
+			</label>
+		
+			<label for="password">
+				<p class="m-2 text-xl text-center">Password</p>
+				<input type="password" name="password" id="" class="h-10 text-lg border-2 border-gray-200 outline-none focus:outline-none focus:border-gray-400 rounded-lg" />
+			</label>
+		
+			<p class="my-3 flex flex-row justify-center"><button type="submit" class="rounded-full bg-blue-300 p-2 px-5">Log in</button></p>
+		</form>
+	</div>
+</div>
 
 {#if form?.success === false}
 	<p>Error: {form.error}</p>
--- src/routes/(app)/auth/signup/+page.svelte
+++ src/routes/(app)/auth/signup/+page.svelte
@@ -2,19 +2,25 @@
 	export let form: FormData;
 </script>
 
-<form method="post">
-	<label for="username">
-		Username
-		<input type="text" name="username" />
-	</label>
+	
 
-	<label for="password">
-		Password
-		<input type="text" name="password" />
-	</label>
-
-	<button type="submit">Sign up</button>
-</form>
+<div class="grid place-items-center h-max">
+	<div class="shadow-xl p-6 rounded-xl">
+		<form method="post">
+			<label for="username">
+				<p class="text-xl text-center my-2">Username</p>
+				<p><input type="text" name="username" class="h-10 text-lg border-2 border-gray-200 outline-none focus:outline-none focus:border-gray-400 rounded-lg" /></p>
+			</label>
+		
+			<label for="password">
+				<p class="text-xl text-center my-2">Password</p>
+				<p><input type="text" name="password" class="h-10 text-lg border-2 border-gray-200 outline-none focus:outline-none focus:border-gray-400 rounded-lg" /></p>
+			</label>
+		
+			<p class="flex flex-row justify-center my-3"><button type="submit" class="rounded-full bg-blue-300 p-2 px-5">Sign up</button></p>
+		</form>
+	</div>
+</div>
 
 {#if form?.success === false}
 	<p>Error: {form.error}</p>
