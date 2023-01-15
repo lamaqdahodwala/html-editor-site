d4501c3551f02c609caaf1e551df67efc1512f81
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-14 19:13:44 -0500

    it works

--- src/routes/(app)/posts/+page.svelte
+++ src/routes/(app)/posts/+page.svelte
@@ -1,1 +1,11 @@
-<p>Posts will show up here</p>
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+    export let data: PageData
+</script>
+
+{#each data.posts as i}
+    <a href="/posts/post/{i.id}">{i.title}</a>
+{:else}
+    <p>Posts will show up here</p>
+{/each}
