57931bc60f70b11be09e67e017ad461202f52303
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-16 16:50:18 -0500

    added new css

--- src/routes/(app)/posts/+page.svelte
+++ src/routes/(app)/posts/+page.svelte
@@ -4,8 +4,9 @@
 	export let data: PageData;
 </script>
 
+<p class="text-2xl">Posts</p>
 {#each data.posts as i}
-	<a href="/posts/post/{i.id}">{i.title}</a>
+	<p class="shadow-lg rounded-xl p-3"><a href="/posts/post/{i.id}">{i.title}</a></p>
 {:else}
 	<p>Posts will show up here</p>
 {/each}
--- src/routes/(app)/posts/post/[id]/+page.svelte
+++ src/routes/(app)/posts/post/[id]/+page.svelte
@@ -4,7 +4,10 @@
 	export let data: PageData;
 </script>
 
-<h1>{data.post.title}</h1>
-<a href="/editor/{data.post.owner.username}/{data.post.pen.title}">Editor view</a>
-<p>Written by {data.post.owner.username}</p>
+<h1 class="text-2xl font-bold">{data.post.title}</h1>
+<p class="text-gray-500 text-sm">Written by {data.post.owner.username}</p>
+<a href="/editor/{data.post.owner.username}/{data.post.pen.title}">View in editor</a>
+<hr class="my-3">
 <p>{data.post.content}</p>
+
+<a href="/posts">Back</a>
