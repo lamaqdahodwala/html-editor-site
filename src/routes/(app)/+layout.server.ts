81bb3bed83ba96bad0ec18a698c58638245564d4
Lamaq Dahodwala <lamaq091008@gmail.com>
2023-01-16 17:28:45 -0500

    added link in navbar

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,5 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 import * as jose from 'jose';
+import { PrismaClient } from '@prisma/client';
 export const load: LayoutServerLoad = async (event) => {
 	const jwt = event.cookies.get('jwt');
 
@@ -13,9 +14,21 @@ export const load: LayoutServerLoad = async (event) => {
 	try {
 		const decoded = await jose.jwtVerify(jwt, new TextEncoder().encode(process.env['KEY']));
 
+		let prisma = new PrismaClient()
+
+		let user = await prisma.user.findUnique({
+			where: {
+				username: String(decoded.payload.aud)
+			}, 
+			select: {
+				id: true
+			}
+		})
+
 		return {
 			logged_in: true,
-			user: decoded.payload.aud
+			user: decoded.payload.aud,
+			user_id: user?.id
 		};
 	} catch (error) {
 		return {
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 	import '../../app.postcss';
+	import type { LayoutData, PageData } from './$types';
+	export let data: LayoutData
 </script>
 
+<nav class="flex flex-row justify-between m-2">
+	<div class="flex flex-row gap-3 items-center">
+		<a href="/" class="text-3xl">HTML Editor site</a>
+		<a href="/" class="text-gray-500">Home</a>
+		<a href="/posts" class="text-gray-500">Recent posts</a>
+	</div>
+	<div class="flex flex-row gap-3 items-center">
+		{#if data.logged_in}
+			<a href="/users/{data.user_id}" class="text-gray-500">Logged in as {data.user}</a>
+		{:else}
+			<a href="/auth/login" class="text-gray-500">Log in</a>
+			<a href="/auth/signup" class="text-gray-500">Sign up</a>
+		{/if}
+	</div>
+</nav>
 <div class="p-3">
 	<slot />
 </div>
